b91bbd11fce8ff81770d0ec45e9dec0cfb38a476
Akshaya0026 <akshayaarigela0405@gmail.com>
2025-12-12 17:03:00 +0530

    Remove WarmUp Assignment from Certify repo

--- OneDrive/Desktop/WarmUp Assignment/backend/src/routes/company.routes.js
+++
@@ -1,9 +0,0 @@
-﻿const express = require("express");
-const router = express.Router();
-const companyController = require("../controllers/company.controller");
-const { verifyFirebaseToken } = require("../middleware/auth.middleware");
-
-router.post("/", verifyFirebaseToken, companyController.createCompany);
-router.get("/", companyController.listCompanies);
-
-module.exports = router;
